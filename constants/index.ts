568f3ef7a536820e833eb8ce172efe3a5d323a26
alisherjan701 <abdulayevalisher987@gmail.com>
2025-07-09 16:59:39 +0500

    new index.ts

--- constants/index.ts
+++ constants/index.ts
@@ -2,7 +2,7 @@ import { Contact, Home, ListVideo, Rss } from "lucide-react"
 
 export const  navLinks = [
     {route:'',name:'Home',icon:Home},
-    {route:'courses',name:'courses',icon:ListVideo},
+    {route:'courses',name:'Courses',icon:ListVideo},
     {route:'blogs',name:'Blogs',icon:Rss},
     {route:'contact',name:'Contact',icon:Contact},
 
